d34520db95e6133112422388f61153e429830cf1
Chris Glass <chris@exoscale.ch>
2018-10-09 13:24:46 +0200

    Add a retries flag.

--- main.go
+++ main.go
@@ -28,11 +28,11 @@ func md5sum(filePath string) (result string, err error) {
 	return
 }
 
-func doDownload(url string, destination string, try int, numberOfRetries int) (err error) {
+func doDownload(url string, destination string, try int, retries int) (err error) {
 	// Create an output file for the local image.
 	out, err := os.Create(destination)
 	if err != nil {
-		fmt.Errorf("Error creating %s. Try %d of %d", destination, try, numberOfRetries)
+		fmt.Errorf("Error creating %s. Try %d of %d", destination, try, retries)
 		return
 	}
 	defer out.Close()
@@ -40,7 +40,7 @@ func doDownload(url string, destination string, try int, numberOfRetries int) (e
 	// Open the URL and get its contents.
 	resp, err := http.Get(url)
 	if err != nil {
-		fmt.Errorf("Error downloading %s. Try %d of %d", url, try, numberOfRetries)
+		fmt.Errorf("Error downloading %s. Try %d of %d", url, try, retries)
 		return
 	}
 	defer resp.Body.Close()
@@ -48,7 +48,7 @@ func doDownload(url string, destination string, try int, numberOfRetries int) (e
 	// Get the file on disk.
 	_, err = io.Copy(out, resp.Body)
 	if err != nil {
-		fmt.Errorf("Error copying file. Try %d of %d", try, numberOfRetries)
+		fmt.Errorf("Error copying file. Try %d of %d", try, retries)
 		return
 	}
 
@@ -58,11 +58,11 @@ func doDownload(url string, destination string, try int, numberOfRetries int) (e
 
 }
 
-func downloadWithRetry(url string, destination string, checksum string) (err error) {
+func downloadWithRetry(url string, destination string, checksum string, retries int) (err error) {
 
 	fmt.Println("Beginning download for", url)
-	for try := 1; try <= numberOfRetries; try++ {
-		err := doDownload(url, destination, try, numberOfRetries)
+	for try := 1; try <= retries; try++ {
+		err := doDownload(url, destination, try, retries)
 		if err != nil {
 			continue
 		}
@@ -71,7 +71,7 @@ func downloadWithRetry(url string, destination string, checksum string) (err err
 		md5sum, err := md5sum(destination)
 
 		if md5sum != checksum {
-			fmt.Errorf("Wrong checksum for file %s. Got %s instead of %s. Try %d of %d", destination, md5sum, checksum, try, numberOfRetries)
+			fmt.Errorf("Wrong checksum for file %s. Got %s instead of %s. Try %d of %d", destination, md5sum, checksum, try, retries)
 			continue
 		}
 
@@ -89,17 +89,18 @@ func main() {
 	fileUrl := flag.String("url", "", "The URL for the file to download.")
 	destination := flag.String("destination", "", "The location on the file system where the file should be downloaded to.")
 	checksum := flag.String("checksum", "", "The MD5 checksum the file is expected to have.")
+	retries := flag.Int("retries", 3, "The number of times the program will retry downloads (defaults to 3).")
 
 	flag.Parse()
 
 	if *fileUrl == "" || *destination == "" || *checksum == "" {
 		fmt.Println("Debbie downer. A program to download things in a cloud environment.")
-		fmt.Println("All arguments are mandatory.")
+		fmt.Println("All arguments are mandatory except retries.")
 		flag.PrintDefaults()
 		return
 	}
 
-	err := downloadWithRetry(*fileUrl, *destination, *checksum)
+	err := downloadWithRetry(*fileUrl, *destination, *checksum, *retries)
 	if err != nil {
 		fmt.Errorf("ERROR: %s", err)
 	}
